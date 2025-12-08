571502ee2329463d7ac1a2d106af079d7f62750e
Mekki-Amine <amine.mekki@sesame.com.tn>
2025-12-08 11:21:31 +0100

    lastcommittttyyhhy

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -10,7 +10,6 @@ if (typeof globalThis.crypto === 'undefined') {
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Electro/FrontEnd',
   build: {
     outDir: 'dist',
   },

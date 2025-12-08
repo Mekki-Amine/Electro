08e9f604abc7431e4fbf6f7a8aa9d3e57c7106fe
Mekki-Amine <amine.mekki@sesame.com.tn>
2025-12-08 20:01:27 +0100

    hhhhgfffd

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://electro-433v.onrender.com',
+        target: 'http://localhost:9090',
         changeOrigin: true,
         secure: false,
       },
